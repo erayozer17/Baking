12f1b8c783aaaeee0abafc667bd62d49333ff1f3
erayozer17 <eray.ozer.91@gmail.com>
2018-05-18 02:09:45 +0200

    tests added

---
+++ app/src/androidTest/java/com/erayo/baking/espresso/MainActivityTest.java
@@ -0,0 +1,68 @@
+package com.erayo.baking.espresso;
+
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.erayo.baking.MainActivity;
+import com.erayo.baking.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    private static final String NUTELLA_PIE = "Nutella Pie";
+    private static final String STARTING_PREP = "Starting prep";
+    private static final int SLEEP_TIME = 5000;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+
+    @Test
+    public void shouldShowCorrectRVWhenAppLaunch(){
+        onView(withId(R.id.main_recycler_view))
+                .check(matches(hasDescendant(withText(NUTELLA_PIE))));
+    }
+
+    @Test
+    public void shouldShowCorrectRVAndContentWhenAnItemIsClicked(){
+        onView(withId(R.id.main_recycler_view))
+                .perform(RecyclerViewActions.actionOnItem(hasDescendant(withText(NUTELLA_PIE)), click()));
+        sleep();
+        onView(withId(R.id.recipe_steps_rv))
+                .check(matches(hasDescendant(withText(STARTING_PREP))));
+    }
+
+    @Test
+    public void shouldDirectToStepDetailWhenAStepClicked(){
+        onView(withId(R.id.main_recycler_view))
+                .perform(RecyclerViewActions.actionOnItem(hasDescendant(withText(NUTELLA_PIE)), click()));
+        sleep();
+        onView(withId(R.id.recipe_steps_rv))
+                .perform(RecyclerViewActions.actionOnItem(hasDescendant(withText(STARTING_PREP)), click()));
+        sleep();
+        onView(withId(R.id.step_long_description_tv)).check(matches(isDisplayed()));
+    }
+
+    public void sleep(){
+        try {
+            Thread.sleep(SLEEP_TIME);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
